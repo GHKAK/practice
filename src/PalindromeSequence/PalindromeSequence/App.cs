bb2434d81aa2b150515b9d47427061c4d1e2bbc6
GHKAK <87258919+GHKAK@users.noreply.github.com>
2023-04-24 13:33:34 +0300

    Palindromes task5 ready

---
+++ src/PalindromeSequence/PalindromeSequence/App.cs
@@ -0,0 +1,33 @@
+﻿using PalindromeSequence;
+
+class App {
+    static List<int> FindPalindromes(int end) {
+
+        return FindPalindromes(0, end);
+    }
+    static List<int> FindPalindromes(int start, int end) {
+        List<int> palindromes = new List<int>();
+        for(int num = start; num <= end; num++) {
+            bool isPalindrome = true;
+            string stringNumber = num.ToString();
+            for(int i = 0; i < Math.Ceiling((double)stringNumber.Length / 2); i++) {
+                if(stringNumber[i] != stringNumber[^(i + 1)]) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if(isPalindrome) {
+                palindromes.Add(num);
+            }
+        }
+        return palindromes;
+    }
+    static void Main() {
+        while(true) {
+            Console.WriteLine("Enter an integer from 0 to 100");
+            int n = ConsoleInput.GetNumber();
+            Console.WriteLine($"Palindromes in range [0,{n}]:  {String.Join(" ", FindPalindromes(n))}");
+        }
+    }
+
+}
---
+++ src/PalindromeSequence/PalindromeSequence/InputParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeSequence {
+    public class InputParser {
+        public static int ParseToInt(string input) {
+            int number;
+            if(Int32.TryParse(input, out number)) {
+                return number;
+            } else {
+                throw new ArgumentException("Wrong Number Format");
+            }
+        }
+    }
+}
