995fffc6b37022e95703893bc5ffaaaf59be81f8
Andrew <brewandbutt@gmail.com>
2023-05-02 15:31:15 +0300

    guide layout

--- src/WeatherForecast/Program.cs
+++ src/WeatherForecast/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList")); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,6 +18,9 @@ if (app.Environment.IsDevelopment()) {
     app.UseSwaggerUI();
 }
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+        
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
