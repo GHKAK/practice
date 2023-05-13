d4b6f77c6000e2e3a42e922f1a1f59a97df4c86c
GHKAK <87258919+GHKAK@users.noreply.github.com>
2023-05-13 23:31:37 +0300

    full merged conflicts search async

--- src/Passports/Repositories/LocalRepository.cs
+++ src/Passports/Repositories/LocalRepository.cs
@@ -8,6 +8,7 @@ using Microsoft.OpenApi.Models;
 using Passports.Models;
 using System;
 using System.Globalization;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -18,7 +19,10 @@ namespace Passports.Repositories {
         private FileInfo _bzFileInfo = new FileInfo($@"C:\Users\{System.Environment.UserName}\Downloads\list_of_expired_passports.csv.bz2");
         private FileInfo _csvFileInfo = new FileInfo($@"C:\Users\{System.Environment.UserName}\Downloads\list_of_expired_passports.csv");
         private List<Passport> _records = new();
-        public string readedStr;
+        private const int TASK_LIMIT = 15;
+        private Task<(string[], int)>[] Tasks { get; set; }
+        private Dictionary<int, string[]> ConflictsDictionary { get; set; }
+        private Dictionary<int, int> TasksOrderMap { get; set; }
         public List<Passport> ReadAll() {
             var configuration = new CsvConfiguration(CultureInfo.InvariantCulture) {
                 HasHeaderRecord = true,
@@ -26,13 +30,13 @@ namespace Passports.Repositories {
                 Delimiter = ",",
                 MissingFieldFound = null,
                 ReadingExceptionOccurred = args => {
-                    if (args.Exception is CsvHelper.FieldValidationException)
+                    if(args.Exception is CsvHelper.FieldValidationException)
                         return false;
                     return true;
                 }
             };
-            using (var reader = new StreamReader(_csvFileInfo.FullName, Encoding.UTF8))
-            using (var csv = new CsvReader(reader, configuration)) {
+            using(var reader = new StreamReader(_csvFileInfo.FullName, Encoding.UTF8))
+            using(var csv = new CsvReader(reader, configuration)) {
                 csv.Context.RegisterClassMap<PassportMap>();
                 var records = csv.GetRecords<Passport>();
                 _records = records.ToList();
@@ -44,22 +48,22 @@ namespace Passports.Repositories {
             int headersOffset = 26;
             int match = 0;
             string remainedRow = String.Empty;
-            using (FileStream fs = new FileStream(_csvFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.None, 524288)) {
+            using(FileStream fs = new FileStream(_csvFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.None, 524288)) {
                 byte[] buffer = new byte[headersOffset];
                 int bytesRead;
                 bool addRow = false;
                 bytesRead = fs.Read(buffer, 0, headersOffset);
                 buffer = new byte[chunkSize];
-                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0) {
+                while((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0) {
                     string chunk;
                     chunk = System.Text.Encoding.Default.GetString(buffer, 0, bytesRead);
                     string[] rows = GetRowsFromChunk(chunk);
                     var lastRowSplitted = rows[^1].Split(",");
-                    if (addRow) {
+                    if(addRow) {
                         AddRemainedRow(rows, remainedRow);
                         addRow = false;
                     }
-                    if (lastRowSplitted.Length != 2 || lastRowSplitted[0].Length != 4 || lastRowSplitted[1].Length != 6) {
+                    if(lastRowSplitted.Length != 2 || lastRowSplitted[0].Length != 4 || lastRowSplitted[1].Length != 6) {
                         remainedRow = rows[rows.Length - 1];
                         Array.Resize(ref rows, rows.Length - 1);
                         addRow = true;
@@ -73,19 +77,19 @@ namespace Passports.Repositories {
             int count = 0;
             int rowsToRead = 118319374;
             int taskLimit = 10;
-            Task<int>[] tasks = new Task<int>[taskLimit]; 
-            using (FileStream fs = new FileStream(_csvFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.None, 32768)) {
-                int rowsPerTask = rowsToRead/ taskLimit;
-                for (int i = 0; i < taskLimit; i++) {
+            Task<int>[] tasks = new Task<int>[taskLimit];
+            using(FileStream fs = new FileStream(_csvFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.None, 32768)) {
+                int rowsPerTask = rowsToRead / taskLimit;
+                for(int i = 0; i < taskLimit; i++) {
                     int start = i * rowsPerTask;
-                    int finish = i+1 * rowsPerTask;
-                    if (i == taskLimit - 1) {
+                    int finish = i + 1 * rowsPerTask;
+                    if(i == taskLimit - 1) {
                         finish = rowsToRead;
                     }
                     tasks[i] = ReadChunkAsync(fs, start, finish);
                 }
                 Task.WaitAll(tasks);
-                foreach (var task in tasks) {
+                foreach(var task in tasks) {
                     count += task.Result;
                 }
             }
@@ -94,10 +98,10 @@ namespace Passports.Repositories {
         private async Task<int> ReadChunkAsync(FileStream fs, int start, int finish) {
             using var reader = new StreamReader(fs);
             int count = 0;
-            for (int i = 0; i < start; i++) {
+            for(int i = 0; i < start; i++) {
                 string? line = reader.ReadLine();
             }
-            for (int i = start; i < finish; i++) {
+            for(int i = start; i < finish; i++) {
                 string? line = reader.ReadLine();
                 count++;
             }
@@ -107,11 +111,10 @@ namespace Passports.Repositories {
             int chunkSize = 120000;
             int headersOffset = 26;
             int match = 0;
-            int taskLimit = 15;
-                Task<(string[], int)>[] tasks = new Task<(string[], int)>[taskLimit];
-                Dictionary<int, string[]> conflictsDictionary = new();
-                Dictionary<int,int> tasksOrderMap=new();
-            using (FileStream fs = new FileStream(_csvFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.None, 262144)) {
+            Tasks = new Task<(string[], int)>[TASK_LIMIT];
+            ConflictsDictionary = new();
+            TasksOrderMap = new();
+            using(FileStream fs = new FileStream(_csvFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.None, 262144)) {
                 byte[] buffer = new byte[headersOffset];
                 int bytesRead;
                 bytesRead = fs.Read(buffer, 0, headersOffset);
@@ -119,62 +122,66 @@ namespace Passports.Repositories {
 
                 int i = 0;
                 int index = 0;
-                while ((bytesRead = await fs.ReadAsync(buffer, 0, buffer.Length)) > 0) {
-                        byte[] bytesBuffer = new byte[bytesRead];
-                        Array.Copy(buffer, 0, bytesBuffer, 0, bytesRead);
-                    if (i < taskLimit) {
-                        var findTask =Task.Run(()=>ProcessChunk(bytesBuffer, bytesRead, series, number));
-                        tasks[i] = findTask;
-                        tasksOrderMap.Add(i, i);
+                while((bytesRead = await fs.ReadAsync(buffer, 0, buffer.Length)) > 0) {
+                    byte[] bytesBuffer = new byte[bytesRead];
+                    Array.Copy(buffer, 0, bytesBuffer, 0, bytesRead);
+                    if(i < TASK_LIMIT) {
+                        var findTask = Task.Run(() => ProcessChunk(bytesBuffer, bytesRead, series, number));
+                        Tasks[i] = findTask;
+                        TasksOrderMap.Add(i, i);
+                        i++;
                     } else {
-                         index = Task.WaitAny(tasks);
-                        (string[] problemRows, int matches) = tasks[index].Result;
-                        conflictsDictionary.Add(tasksOrderMap[index],problemRows);
-                        match += matches;
-                        tasks[index] = Task.Run(() => ProcessChunk(bytesBuffer, bytesRead, series, number));
-                        tasksOrderMap[index] = i;
+                        index = Task.WaitAny(Tasks);
+                        match += MatchTaskRoutine(ref i, index);
+                        Tasks[index] = Task.Run(() => ProcessChunk(bytesBuffer, bytesRead, series, number));
                     }
                     buffer = new byte[chunkSize];
-                    i++;
                 }
 
-                Task.WaitAll(tasks);
-                for (int l = 0; l < tasks.Length; l++) {
-                    if (l == index) {
+                Task.WaitAll(Tasks);
+                for(int l = 0; l < Tasks.Length; l++) {
+                    if(l == index) {
                         continue;
                     }
-                    (string[] problemRows, int matches) = tasks[l].Result;
-                    conflictsDictionary.Add(tasksOrderMap[l], problemRows);
-                    match += matches;
-
-
-                    tasksOrderMap[l] = i;
-                    i++;
+                    match += MatchTaskRoutine(ref i, l);
                 }
-                var mergedConflicts = Task.Run(() => GetRowsList(conflictsDictionary));
+                var mergedConflicts = Task.Run(() => GetRowsList(ConflictsDictionary));
                 mergedConflicts.Wait();
                 var list = mergedConflicts.Result;
-                match +=FindInMatchesInConflicts(list, series, number);
+                match += FindInMatchesInConflicts(list, series, number);
             }
             return match;
         }
+        private int MatchTaskRoutine(ref int i, int taskIndex) {
+            (string[] problemRows, int matches) = Tasks[taskIndex].Result;
+            ConflictsDictionary.Add(TasksOrderMap[taskIndex], problemRows);
+            TasksOrderMap[taskIndex] = i;
+            i++;
+            return matches;
+        }
+
         private int FindInMatchesInConflicts(List<string> conflicts, int series, int number) {
             int matches = 0;
             string matchString = series + "," + number;
-            for (int i = 0; i < conflicts.Count; i++) {
-                if (conflicts[i] == matchString) {
+            for(int i = 0; i < conflicts.Count; i++) {
+                if(conflicts[i] == matchString) {
                     matches++;
                 }
             }
             return matches;
         }
-        private List<string> GetRowsList(Dictionary<int, string[]> conflicts) { 
+        private List<string> GetRowsList(Dictionary<int, string[]> conflicts) {
             List<string> rows = new List<string>();
-            for (int i = 0; i < conflicts.Count; i++) {
-                foreach (var row in conflicts[i]) {
-                    if (row != null) {
-                        //merge here
-                        rows.Add(row);
+            for(int i = 0; i < conflicts.Count; i++) {
+                string firstRow = conflicts[i][0];
+                string secondRow = conflicts[i][1];
+                if(firstRow.Length<11) {
+                    firstRow = conflicts[i - 1][1] + firstRow;
+                }
+                rows.Add(firstRow);
+                if(secondRow!=null) {
+                    if(secondRow.Length == 11) {
+                        rows.Add(secondRow);
                     }
                 }
             }
@@ -194,17 +201,17 @@ namespace Passports.Repositories {
         private int MatchesInRowsCutted(string[] rows, int series, int number) {
             int match = 0;
             int seriesData, numberData;
-            for (int i = 1; i < rows.Length - 1; i++) {
+            for(int i = 1; i < rows.Length - 1; i++) {
                 var row = rows[i];
                 var passportData = row.Split(",");
-                if (passportData.Length != 2) { 
-                
+                if(passportData.Length != 2) {
+
                 }
-                if (!int.TryParse(passportData[0], out seriesData)
+                if(!int.TryParse(passportData[0], out seriesData)
                     || !int.TryParse(passportData[1], out numberData)) {
                     continue;
                 }
-                if (series == seriesData && number == numberData) {
+                if(series == seriesData && number == numberData) {
                     match++;
                 }
             }
@@ -225,13 +232,13 @@ namespace Passports.Repositories {
         public int MatchesInRows(string[] rows, int series, int number) {
             int match = 0;
             int seriesData, numberData;
-            foreach (var row in rows) {
+            foreach(var row in rows) {
                 var passportData = row.Split(",");
-                if (!int.TryParse(passportData[0], out seriesData)
+                if(!int.TryParse(passportData[0], out seriesData)
                     || !int.TryParse(passportData[1], out numberData)) {
                     continue;
                 }
-                if (series == seriesData && number == numberData) {
+                if(series == seriesData && number == numberData) {
                     match++;
                 }
             }
@@ -239,54 +246,54 @@ namespace Passports.Repositories {
         }
 
         public void DecompressFile() {
-            using (FileStream fileToDecompressAsStream = _bzFileInfo.OpenRead()) {
+            using(FileStream fileToDecompressAsStream = _bzFileInfo.OpenRead()) {
                 string decompressedFileName = _csvFileInfo.FullName;
-                using (FileStream decompressedStream = File.Create(decompressedFileName)) {
+                using(FileStream decompressedStream = File.Create(decompressedFileName)) {
                     try {
                         BZip2.Decompress(fileToDecompressAsStream, decompressedStream, true);
-                    } catch (Exception ex) {
+                    } catch(Exception ex) {
                         throw;
                     }
                 }
             }
         }
         public async Task<int> GetLinesCountAsync() {
-            if (string.IsNullOrEmpty(_csvFileInfo.FullName))
+            if(string.IsNullOrEmpty(_csvFileInfo.FullName))
                 throw new ArgumentException("Parameter should not be null or empty!", nameof(_csvFileInfo.FullName));
             using var reader = File.OpenText(_csvFileInfo.FullName);
             int count = 0;
-            while (true) {
+            while(true) {
                 string? line = await reader.ReadLineAsync().ConfigureAwait(false);
-                if (line == null)
+                if(line == null)
                     break;
                 count++;
             }
             return count;
         }
         public async Task<int> GetLinesCount2Async() {
-            if (string.IsNullOrEmpty(_csvFileInfo.FullName))
+            if(string.IsNullOrEmpty(_csvFileInfo.FullName))
                 throw new ArgumentException("Parameter should not be null or empty!", nameof(_csvFileInfo.FullName));
             using var fs = new FileStream(_csvFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.None, 32768, true);
             using var reader = new StreamReader(fs);
             int count = 0;
-            while (true) {
+            while(true) {
                 string? line = await reader.ReadLineAsync().ConfigureAwait(false);
-                if (line == null)
+                if(line == null)
                     break;
                 count++;
             }
             return count;
         }
         public Task<int> GetLinesCount3Async() {
-            if (string.IsNullOrEmpty(_csvFileInfo.FullName))
+            if(string.IsNullOrEmpty(_csvFileInfo.FullName))
                 throw new ArgumentException("Parameter should not be null or empty!", nameof(_csvFileInfo.FullName));
             return Task.Run(() => {
                 using var fs = new FileStream(_csvFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.None, 32768);
                 using var reader = new StreamReader(fs);
                 int count = 0;
-                while (true) {
+                while(true) {
                     string? line = reader.ReadLine();
-                    if (line == null) {
+                    if(line == null) {
                         break;
                     }
                     count++;
@@ -299,22 +306,22 @@ namespace Passports.Repositories {
             int headersOffset = 26;
             int count = 0;
             string remainedRow = String.Empty;
-            using (FileStream fs = File.OpenRead(_csvFileInfo.FullName)) {
+            using(FileStream fs = File.OpenRead(_csvFileInfo.FullName)) {
                 byte[] buffer = new byte[headersOffset];
                 int bytesRead;
                 bool addRow = false;
                 bytesRead = fs.Read(buffer, 0, headersOffset);
                 buffer = new byte[chunkSize];
-                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0) {
+                while((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0) {
                     string chunk;
                     chunk = System.Text.Encoding.Default.GetString(buffer, 0, bytesRead);
                     string[] rows = GetRowsFromChunk(chunk);
                     var lastRowSplitted = rows[^1].Split(",");
-                    if (addRow) {
+                    if(addRow) {
                         AddRemainedRow(rows, remainedRow);
                         addRow = false;
                     }
-                    if (lastRowSplitted.Length != 2 || lastRowSplitted[0].Length != 4 || lastRowSplitted[1].Length != 6) {
+                    if(lastRowSplitted.Length != 2 || lastRowSplitted[0].Length != 4 || lastRowSplitted[1].Length != 6) {
                         remainedRow = rows[rows.Length - 1];
                         Array.Resize(ref rows, rows.Length - 1);
                         addRow = true;
