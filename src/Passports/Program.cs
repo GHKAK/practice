737b1400f6dcaf61757e5ed1977f69dca58bd732
Andrew <brewandbutt@gmail.com>
2023-05-23 17:26:30 +0300

    not configured di

--- src/Passports/Program.cs
+++ src/Passports/Program.cs
@@ -4,9 +4,12 @@ using Passports.Data;
 using Passports.Models;
 using Passports.Repositories;
 using Passports.Repositories.Interfaces;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddUserSecrets("ea88d7a2-ed79-45f4-b3a9-0e4670cbe894");   
+builder.Configuration.AddUserSecrets("ea88d7a2-ed79-45f4-b3a9-0e4670cbe894");
+
 // Add services to the container.
 builder.Services.AddEntityFrameworkNpgsql().AddDbContext<PassportContext>(opt => {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("postgresPassports"));
@@ -16,10 +19,10 @@ builder.Services.AddEntityFrameworkNpgsql().AddDbContext<PassportContext>(opt =>
 builder.Services.AddScoped<PostgresRepository>();
 builder.Services.AddScoped<IPassportRepository, LoggingPassportRepository>(provider => {
     var decoratedRepository = provider.GetService<PostgresRepository>();
-    var logger = LoggerFactory.Create(config =>
-    {
-        config.AddConsole();
-    }).CreateLogger("Program");
+    var logger =  new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new[]{new Uri("http://localhost:5601/")}))
+        .CreateLogger();
     return new LoggingPassportRepository(decoratedRepository, logger);
 });
 builder.Services.AddScoped<LocalRepository>();
--- src/Passports/Repositories/LoggingPassportRepository.cs
+++ src/Passports/Repositories/LoggingPassportRepository.cs
@@ -1,42 +1,43 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using Passports.Models;
 using Passports.Repositories.Interfaces;
-
+using Serilog.Core;
 namespace Passports.Repositories; 
 
 public class LoggingPassportRepository : IPassportRepository {
     private readonly IPassportRepository _decoratedRepository;
-    private readonly ILogger _logger;
+    private readonly Serilog.ILogger _logger;
     private readonly Stopwatch _sw;
 
-    public LoggingPassportRepository(IPassportRepository decoratedRepository, ILogger logger) {
+    public LoggingPassportRepository(IPassportRepository decoratedRepository, Serilog.ILogger logger) {
         _decoratedRepository = decoratedRepository;
         _logger = logger;
         _sw = new Stopwatch();
     }
 
     public async Task<Passport?> GetBySeriesNumber(short series, int number) {
-        return await LogAndExecute(()=>_decoratedRepository.GetBySeriesNumber(series,number));
+        return await LogAndExecute(()=>_decoratedRepository.GetBySeriesNumber(series,number),MethodBase.GetCurrentMethod().Name);
     }
 
     public async Task<int> CountActual(bool isActual) {
-        return await LogAndExecute(()=>_decoratedRepository.CountActual(isActual));
+        return await LogAndExecute(()=>_decoratedRepository.CountActual(isActual),MethodBase.GetCurrentMethod().Name);
     }
 
     public async Task<bool> MigrateFromFile() {
-        return await LogAndExecute(_decoratedRepository.MigrateFromFile);
+        return await LogAndExecute(_decoratedRepository.MigrateFromFile,MethodBase.GetCurrentMethod().Name);
     }
-    private async Task<T> LogAndExecute<T>(Func<Task<T>> method)
+    private async Task<T> LogAndExecute<T>(Func<Task<T>> method, string methodName)
     {
         _sw.Restart();
         try {
             var result = await method.Invoke();
             _sw.Stop();            
-            _logger.LogInformation($"{method} finished in {_sw.Elapsed} seconds");
+            _logger.Information($"{methodName} finished in {_sw.Elapsed} seconds");
             return result;
         } catch {
             _sw.Stop();
-            _logger.LogInformation($"{method} finished in {_sw.Elapsed} seconds");
+            _logger.Information($"{methodName} finished in {_sw.Elapsed} seconds");
             throw;
         }
     }
